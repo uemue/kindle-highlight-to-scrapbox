eb8ee9f03df98500fabd0d27473a426e2ed7fd39
uemue <7414847+uemue@users.noreply.github.com>
2023-04-30 18:55:11 +0900

    Refactor index.ts to use async/await for clipboard copy

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
 import { parsePage } from 'kindle-highlight-to-markdown';
 import { ParseResult } from 'kindle-highlight-to-markdown/lib/parse';
 
-const userName = 'your_user_name';
+(async function () {
+  const userName = 'your_user_name';
 
-function convertToScrapbox(parseResult: ParseResult): string {
-  const { title, url, coverImageUrl, annotations } = parseResult;
-  const header = `[${coverImageUrl}]\n[${title} ${url}]`;
-  const lines = annotations.map((annotation) => {
-    let line = `> ${annotation.highlight}`;
-    if (annotation.note) {
-      return line + `\n [${userName}.icon]${annotation.note}`;
+  function convertToScrapbox(parseResult: ParseResult): string {
+    const { title, url, coverImageUrl, annotations } = parseResult;
+    const header = `[${coverImageUrl}]\n[${title} ${url}]`;
+    const lines = annotations.map((annotation) => {
+      let line = `> ${annotation.highlight}`;
+      if (annotation.note) {
+        return line + `\n [${userName}.icon]${annotation.note}`;
+      }
+      return line;
+    });
+    return [header, ...lines].join('\n\n');
+  }
+
+  async function copyToClipboard(text: string): Promise<void> {
+    try {
+      await navigator.clipboard.writeText(text);
+    } catch (err) {
+      console.error('クリップボードへのコピーに失敗しました。', err);
     }
-    return line;
-  });
-  return [header, ...lines].join('\n\n');
-}
+  }
 
-function copyToClipboard(text: string): void {
-  const textarea = document.createElement('textarea');
-  textarea.textContent = text;
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand('copy');
-  document.body.removeChild(textarea);
-}
+  const parseResult = parsePage(window);
+  const scrapboxText = convertToScrapbox(parseResult);
+  await copyToClipboard(scrapboxText);
 
-const parseResult = parsePage(window);
-const scrapboxText = convertToScrapbox(parseResult);
-copyToClipboard(scrapboxText);
-
-alert('ハイライトがScrapbox形式でコピーされました！');
+  alert('ハイライトがScrapbox形式でコピーされました！');
+})();
