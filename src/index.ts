a977cb9969dab0cf6d361380d077f6f87aa0934f
uemue <7414847+uemue@users.noreply.github.com>
2023-04-30 22:13:29 +0900

    Add a link to the Kindle app location number.
    Add a link to the specific location number within the Kindle app if `kindleProxy` is set.
    For more information about kindleProxy, please visit https://efcl.info/2022/01/29/kindle-highlight-to-markdown/

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,34 @@ import { parsePage } from 'kindle-highlight-to-markdown';
 import { ParseResult } from 'kindle-highlight-to-markdown/lib/parse';
 
 (async function () {
+  // replace your_user_name to your Scrapbox user name
   const userName = 'your_user_name';
+  // replace kindleProxy to your kindle proxy url. (e.g. https://xxx.workers.dev/)
+  const kindleProxy = '';
 
   function convertToScrapbox(parseResult: ParseResult): string {
     const { title, url, coverImageUrl, annotations } = parseResult;
     const header = `[${coverImageUrl}]\n[${title} ${url}]`;
-    const lines = annotations.map((annotation) => {
-      let line = `> ${annotation.highlight}`;
-      if (annotation.note) {
-        return line + `\n [${userName}.icon]${annotation.note}`;
+    const highlights = annotations.map((annotation) => {
+      let highlight = `>${annotation.highlight}`;
+
+      // Add Kindle Location Link if kindleProxy is set
+      if (kindleProxy !== '') {
+        const locationURL = annotation.kindleUrl.replace(
+          'kindle://',
+          kindleProxy
+        );
+        highlight += `\n>Location: [${annotation.locationNumber} ${locationURL}]`;
       }
-      return line;
+
+      // Add Note if exists
+      if (annotation.note) {
+        highlight += `\n [${userName}.icon]${annotation.note}`;
+      }
+
+      return highlight;
     });
-    return [header, ...lines].join('\n\n');
+    return [header, ...highlights].join('\n\n');
   }
 
   async function copyToClipboard(text: string): Promise<void> {
